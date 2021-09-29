ee62d2a0a3eb13b0618f2c00777bcb0b0750520e
David <david.mael.cormier@gmail.com>
2021-09-29 02:29:39 +0100

    preprocessing of letter better reflects training data

--- backend/model/preprocess.py
+++ backend/model/preprocess.py
@@ -9,8 +9,6 @@ def textDetect(imagepath):
     image = imageRGB.load()
     width, height = imagefile.size
 
-    print("dimensions:", width, height)
-
     lineTop = None
     linebounds = []
 
@@ -27,39 +25,23 @@ def textDetect(imagepath):
                 #find top of text line
                 if lineTop == None:
                     lineTop = scanline
-                    print("top detected at:", scanline)
 
         if writingDetected == False and lineTop != None:
-
-            print("bottom detected at:", scanline)
 
             #only keep elements that have a height of 2.5% of the screen height (about 45 px on my screen)
             if( (scanline - lineTop)/height > 0.025):
 
-                for i in range(width):
-                    image[i, lineTop] = (255, 0, 0)
-                    image[i, scanline] = (0, 255, 0)
-
                 linebounds.append((lineTop, scanline))
-            
-            else:
-
-                for i in range(width):
-                    image[i, lineTop] = (255, 170, 00)
-                    image[i, scanline] = (119, 255, 0)
-
 
             lineTop = None
-
-    #imageRGB.show()
 
     return linebounds
 
 #return the number of characters
 def charSegment(imagepath, linebounds):
 
-    print(linebounds)
-    print(len(linebounds))
+    #print(linebounds)
+    #print(len(linebounds))
 
     imagefile = Image.open(imagepath)
     imageRGB = imagefile.convert('RGB')
@@ -87,20 +69,30 @@ def charSegment(imagepath, linebounds):
                         
             if charDetected == False and charLeft != None:
 
-                for i in range( linebounds[line][0] , linebounds[line][1] , 1 ):
-                    image[charLeft, i] = (255, 0, 0)
-                    image[column, i] = (0, 255, 0)
+                characterTop = None
+                characterBottom = None
+                for row in range (linebounds[line][0] , linebounds[line][1] , 1):
+                    blankRow = True
+                    for c in range (charLeft, column, 1):
+                        if image[c, row] != (255, 255, 255):
+                            blankRow = False
+                            if characterTop == None:
+                                characterTop = row
+                    if blankRow == True and characterBottom == None and characterTop != None:
+                        characterBottom = row
+                if characterBottom == None:
+                    characterBottom = linebounds[line][1]
 
-                cropped = imagefile.crop((charLeft, linebounds[line][0], column, linebounds[line][1]))
-                if linebounds[line][1] - linebounds[line][0] > column - charLeft:
-                    resizedW = floor((column - charLeft)*28/(linebounds[line][1] - linebounds[line][0]))
-                    resizedH = 28
+                
+                cropped = imagefile.crop((charLeft, characterTop, column, characterBottom))
+                if characterBottom - characterTop > column - charLeft:
+                    resizedW = floor((column - charLeft)*25/(characterBottom - characterTop))
+                    resizedH = 25
                 else:
-                    resizedW = 28
-                    resizedH = floor((linebounds[line][1] - linebounds[line][0])*28/(column - charLeft))
+                    resizedW = 25
+                    resizedH = floor((characterBottom - characterTop)*25/(column - charLeft))
                 resized = cropped.resize(( resizedW , resizedH))
                 blank = Image.new('RGB', (28, 28), (255, 255, 255))
-                #print(14 - floor(resizedW/2))
                 blank.paste(resized, (14 - floor(resizedW/2), 14 - floor(resizedH/2) ))
                 try:
                     blank.save( ('character-images/'+str(imageCtr)+'.png'), 'png')
@@ -110,7 +102,5 @@ def charSegment(imagepath, linebounds):
                 imageCtr = imageCtr + 1
 
                 charLeft = None
- 
-    #imageRGB.show()
 
     return imageCtr
